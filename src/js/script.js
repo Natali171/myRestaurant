5651e9c8fda454e471b3b64d11e16463c4146752
Natalia Andriichuk <natali1sitnik@gmail.com>
2023-08-06 13:14:51 +0300

    06.08

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,43 @@
-// console.log("TEST");
+const menuBtn = $(".header__menu-button");
+const darkOverlay = $(".dark-overlay");
+const mobMenu = $(".mob-menu");
+
+$(document).on("click", ".header__menu-button", handleMenu);
+$(document).on("click", ".mob-menu__is-submenu", handleToggleMenu);
+
+function handleMenu(e) {
+  e.preventDefault();
+  darkOverlay.toggleClass("visible");
+  mobMenu.toggleClass("visible");
+}
+function handleToggleMenu(e) {
+  e.preventDefault();
+  const $this = $(this);
+  $this.parent(".mob-menu__has-submenu").toggleClass("opened");
+  $this.next("ul").slideToggle(300, function () {
+    $(this)
+      .find(".mob-menu__has-submenu")
+      .removeClass("opened")
+      .children("ul")
+      .hide();
+    $this
+      .parent(".mob-menu__has-submenu")
+      .toggleClass("opened")
+      .siblings("li")
+      .removeClass("opened")
+      .find("ul")
+      .hide();
+  });
+}
+
+function resetMobileMenu() {
+  darkOverlay.removeClass("visible");
+  mobMenu.removeClass("visible");
+  $(".mob-menu__box .mob-menu__has-submenu")
+    .removeClass("opened")
+    .find("ul")
+    .hide();
+}
 
 function initMobile() {
   console.log("is-mobile");
